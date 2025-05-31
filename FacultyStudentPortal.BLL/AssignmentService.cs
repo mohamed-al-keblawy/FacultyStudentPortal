ffe921155ed7996b1bb2d1a4d7c1d48fc6319a36
Mohamed Al Keblawy <alkeblawy.m@gmail.com>
2025-06-01 01:26:34 +0400

    add AssignmentServiceTests

---
+++ FacultyStudentPortal.BLL/AssignmentService.cs
@@ -0,0 +1,22 @@
+﻿using FacultyStudentPortal.DAL.Interfaces;
+using FacultyStudentPortal.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FacultyStudentPortal.BLL
+{
+    public class AssignmentService
+    {
+        private readonly IAssignmentRepository _repo;
+
+        public AssignmentService(IAssignmentRepository repo)
+        {
+            _repo = repo;
+        }
+        public Task<IEnumerable<Assignment>> GetAllAsync() => _repo.GetAllAsync();
+    }
+
+}
---
+++ FacultyStudentPortal.BLL/Services/AssignmentServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using FacultyStudentPortal.BLL;
+using FacultyStudentPortal.DAL.Interfaces;
+using FacultyStudentPortal.Models.Entities;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace FacultyStudentPortal.Tests.Services
+{
+    public class AssignmentServiceTests
+    {
+        [Fact]
+        public async Task GetAllAsync_ReturnsAssignments()
+        {
+            // Arrange
+            var mockRepo = new Mock<IAssignmentRepository>();
+            mockRepo.Setup(r => r.GetAllAsync()).ReturnsAsync(new List<Assignment> {
+                new Assignment { AssignmentId = 1, Title = "Test Assignment" }
+            });
+
+            var service = new AssignmentService(mockRepo.Object);
+
+            // Act
+            var result = await service.GetAllAsync();
+
+            // Assert
+            Assert.Single(result);
+            Assert.Equal("Test Assignment", result.First().Title);
+        }
+    }
+}
